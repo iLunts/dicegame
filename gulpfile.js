0c298a37e71be5aabe210b7e51b12f8d6799f4e2
Roman Luntsevich <shipik2008@gmail.com>
2018-03-16 23:59:47 +0300

    Create thwo block

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿/// <binding AfterBuild='scripts' />
+var gulp = require('gulp');
+
+var requireDir = require('require-dir');
+requireDir('./gulp-tasks');
+
+// ------------------------------------------------------------------------
+
+gulp.task('style', ['css.site', 'css.inline']);
+// gulp.task('scripts', ['scripts.core', 'scripts.iframe', 'scripts.admin', 'scripts.website']);
+// gulp.task('svg', ['svg.min', 'svg.inject']);
+
+gulp.task("watch", function () {
+	gulp.watch(["web-src/less/**/*.less"], ["style"]);
+	// gulp.watch(["ClientApp/app/**/*.*"], ["scripts"]);
+	// gulp.watch(["web-src/svg/*.svg"], ["svg"]);
+});
+
