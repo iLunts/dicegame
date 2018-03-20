96240d7d966f5060805e1c38f02614960d743a2c
Roman Luntsevich <shipik2008@gmail.com>
2018-03-20 19:32:48 +0300

    Run SCSS

--- gulp-tasks/gulp-css.js
+++ gulp-tasks/gulp-css.js
@@ -8,6 +8,7 @@ var clean = require('gulp-clean');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var inlineCss = require('gulp-inline-css');
+var sass = require('gulp-sass');
 
 
 // ---------------------------------------
@@ -35,9 +36,35 @@ gulp.task('css.site', function () {
         .pipe(gulp.dest('content/css/'));
 });
 
+// gulp.task('scss.site', function () {
+//     gulp.src(['html/sass/base/_base.scss'])
+//         .pipe(less({
+//             paths: [path.join(__dirname, 'less', 'includes')]
+//         }))
+//         .pipe(concat('site.min.css'))
+//         .pipe(clean({
+//             force: true
+//         }))
+//         .pipe(cleanCSS({
+//             compatibility: 'ie8'
+//         }))
+//         .pipe(autoprefixer({
+//             browsers: ['last 4 versions'],
+//             cascade: false
+//         }))
+//         .pipe(gulp.dest('content/css/'));
+// });
+
+gulp.task('sass', function () {
+    return gulp.src('html/sass/style/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('html/css/min/'));
+});
+
 
 gulp.task('css.inline', function () {
     return gulp.src('./index.html')
         .pipe(inlineCss())
         .pipe(gulp.dest('build/'));
 });
+
