cc6fc3a09fdb279502dab34615f8a59b6051dc1d
apn2qq <apn2qq@gmail.com>
2018-04-18 12:11:27 +0300

    minify team photo, scripts & main.css

--- js/timeline.js
+++ js/timeline.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-	var countChildTop = $('.timeline__list--top .timeline__item').length;
-	var screenWidth = $(document).width();
-	// var addPaddingLeft = $(document).width();
-	var itemWidth = $('.timeline__item').width();
-	
-	console.log('width: ' + itemWidth);
+// 	var countChildTop = $('.timeline__list--top .timeline__item').length;
+// 	var screenWidth = $(document).width();
+// 	// var addPaddingLeft = $(document).width();
+// 	var itemWidth = $('.timeline__item').width();
 
-});
+// 	console.log('width: ' + itemWidth);
+
+// });
